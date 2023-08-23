738c880945b91cf04dd7dd39d1daea3ca9661990
sharibeast <georginasharif@gmail.com>
2023-08-23 23:28:43 +0300

    adding shell header

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_M
+#define HEADER_M
+
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strdup(char *str);
+
+#endif
