eafd5d1f63708c0a0aece501ee040bd668f902da
sharibeast <georginasharif@gmail.com>
2023-08-24 01:20:09 +0300

    adding header files

--- shell.h
+++ shell.h
@@ -7,6 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
 extern char **envronm;
 
 /**
