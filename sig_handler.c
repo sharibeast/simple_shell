c18c67a4779387fa0fa32a7f8d29ecc48be08a04
Almas <almasemilius@gmail.com>
2023-08-25 01:32:48 +0300

    betty style

---
+++ sig_handler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * sig_handler - checks if Ctrl C is pressed
+ * @sig_num: int
+ */
+void sig_handler(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		_puts("\n#cisfun$ ");
+	}
+}
